e1444b8cc233543d06071f47f557a86701d5a1ae
Thayane Barros <thayanes438@gmail.com>
2023-02-02 20:25:37 -0300

    adicionando segunda parte do curso de javascript

---
+++ JavaScriptII-DOM/Exercicio 11 - Escalação de Times/index.js
@@ -0,0 +1,38 @@
+function addPlayer() {
+  const position = document.getElementById("position").value;
+  const namePlayer = document.getElementById("namePlayer").value;
+  const number = document.getElementById("number").value;
+
+  const confirmation = confirm(
+    "Escalar " + namePlayer + " como " + position + "?"
+  );
+
+  if (confirmation) {
+    const teamList = document.getElementById("teamList");
+
+    const playerItem = document.createElement("li");
+    playerItem.id = "player-" + number;
+    playerItem.innerText = position + ": " + namePlayer + " (" + number + ")";
+
+    teamList.appendChild(playerItem);
+  }
+
+  document.getElementById("position").value = "";
+  document.getElementById("namePlayer").value = "";
+  document.getElementById("number").value = "";
+}
+
+function removePlayer() {
+  const number = document.getElementById("numberToRemove").value;
+  const playerToRemove = document.getElementById("player-" + number);
+
+  const confirmation = confirm(
+    "Remover o jogador " + playerToRemove.innerText + "?"
+  );
+
+  if (confirmation) {
+    playerToRemove.remove();
+  }
+
+  document.getElementById("numberToRemove").value = "";
+}
