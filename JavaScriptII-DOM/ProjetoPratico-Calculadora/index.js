297cc62aa041f1e828455bbfa1fc787861bab631
Thayane Barros <thayanes438@gmail.com>
2023-02-18 20:32:32 -0300

    adicionando o projeto guiado-calc.js

---
+++ JavaScriptII-DOM/ProjetoPratico-Calculadora/index.js
@@ -0,0 +1,104 @@
+const main = document.querySelector("main");
+const root = document.querySelector(":root");
+const input = document.getElementById("input");
+const resulInput = document.getElementById("result");
+const btnCopy = document.getElementById("copyToClipboard");
+
+const allowedKeys = [
+  "(",
+  ")",
+  "/",
+  "*",
+  "-",
+  "+",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  ".",
+  "%",
+  " ",
+];
+
+function calculate() {
+  resulInput.value = "ERROR";
+  resulInput.classList.add("error");
+  const result = eval(input.value);
+  resulInput.value = result;
+  resulInput.classList.remove("error");
+}
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault();
+
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key;
+    return;
+  }
+
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1);
+  }
+
+  if (ev.key === "Enter") {
+    calculate();
+  }
+});
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", function () {
+    const value = charKeyBtn.dataset.value;
+    input.value += value;
+  });
+});
+
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = "";
+  resulInput.value = "";
+  input.focus();
+
+  if (btnCopy.innerText === "Copied!") {
+    btnCopy.innerText = "Copy";
+    btnCopy.classList.remove("success");
+  }
+
+  if (resulInput.classList.contains("error")) {
+    resulInput.classList.remove("error");
+  }
+});
+
+document.getElementById("equal").addEventListener("click", calculate);
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9");
+    root.style.setProperty("--border-color", "#aaa");
+    root.style.setProperty("--font-color", "#212529");
+    root.style.setProperty("--primary-color", "#26834a");
+    main.dataset.theme = "light";
+  } else {
+    root.style.setProperty("--bg-color", "#212529");
+    root.style.setProperty("--border-color", "#666");
+    root.style.setProperty("--font-color", "#f1f5f9");
+    root.style.setProperty("--primary-color", "#4dff91");
+    main.dataset.theme = "dark";
+  }
+});
+
+btnCopy.addEventListener("click", function (ev) {
+  const button = ev.currentTarget;
+  if (button.innerText === "Copy") {
+    button.innerText = "Copied!";
+    button.classList.add("success");
+    navigator.clipboard.writeText(resulInput.value);
+  } else {
+    button.innerText = "Copy";
+    button.classList.remove("success");
+  }
+});
